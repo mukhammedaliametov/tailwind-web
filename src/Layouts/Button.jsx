8e443c23573b51fadc26efe22ed06e96abfcabef
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-07-29 10:27:21 +0500

    update home page

--- src/Layouts/Button.jsx
+++ src/Layouts/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Button = (props) => {
   return (
-    <Link className="text-center px-5 max-w-[180px] cursor-pointer py-2 mt-5 text-bold bg-green-500 rounded-2xl">
+    <Link className="text-black md:px-6 text-center px-4 max-w-[180px] cursor-pointer py-3 text-bold bg-green-500 rounded-md hover:bg-green-300">
       {props.title}
     </Link>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,29 @@
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
 import Button from "../Layouts/Button";
 
 const Home = () => {
   return (
     <>
       <div className="max-w-[800px] w-full  mx-auto text-center ">
-        <p className="text-green-500 font-bold uppercase p-2 mt-[10rem]">
+        <p className="text-green-500 font-bold uppercase p-2 mt-[8rem]">
           Growing With Data Analitycs
         </p>
-        <p className="font-bold p-2 text-3xl">Growing With Data</p>
-        <p className="font-bold px-2 text-2xl">Fast, flexible financing for</p>
-        <p className="font-bold px-2 text-1xl text-black-500 mb-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-          laborum iusto ipsam necessitatibus veniam? Ratione distinctio optio
-          sed. Earum sequi totam rem dolor in aut ipsa fugiat, voluptatum! velit
-          eaque.
+        <h1 className="md:text-6xl sm:text-6xl text-3xl  font-bold px-2 md:py-6">
+          Growing With Data
+        </h1>
+        <p className="md:text-5xl sm:text-4xl text-xl font-bold mb-2">
+          Fast, flexible financing for {' '}
+          <Typewriter
+            words={["BTB", "BTC", "SASS"]}
+            loop={5}
+            cursor
+            cursorStyle="_"
+            typeSpeed={70}
+            deleteSpeed={50}
+          />
         </p>
+        <p className='text-gray-500 font-bold text-xl md:text-2xl mb-6'>Ipsum, dolor sit consectetur adipisicing elit. In maxime nesciunt, veritatis sint laborum repudiandae.</p>
         <Button title={'Get Started'} />
       </div>
     </>
